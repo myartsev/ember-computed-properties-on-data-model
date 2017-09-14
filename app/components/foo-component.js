334d1d23dea26f6dd06c2f0d023d744ef6bd0518
Michael Yartsev <anijap@gmail.com>
2017-09-14 13:51:11 +0900

    Computed data model in a component is not working

--- app/components/foo-component.js
+++ app/components/foo-component.js
@@ -1,6 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  computedModel: Ember.computed('model', function() {
+    return this.get('model').map(function(m) {
+      return `Computed: ${m.data.bar}`;
+    });
+  }),
+
+  computedModelChanged: Ember.observer('model', function() {
+    console.log('computedModelChanged');
+  }),
+
+  init() {
+    this._super(...arguments);
+    this.get('computedModelChanged');
+  },
+
   actions: {
     refreshModel() {
       this.sendAction('invalidateModel');
